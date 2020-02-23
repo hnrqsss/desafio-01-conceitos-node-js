c47e2de4301edef67a7ba6b5cbe880bc0c7307dc
hnrqss <hnrq.assuncao@gmail.com>
2020-02-23 20:04:21 -0300

    finish project

--- src/server.js
+++ src/server.js
@@ -17,8 +17,7 @@ app.post("/projects", reqCount, (req, res) => {
   const { id } = req.body
 
   const project = dbProjects.filter(item => item.id === id)
-  console.log(project)
-
+  
   if(project.length > 0) {
     return res.status(400).json({ "message": "This project already exist!"})
   }
@@ -30,20 +29,48 @@ app.post("/projects", reqCount, (req, res) => {
 
 app.post("/projects/:id/tasks", reqCount, checkProjectExist, (req,res) => {
 
+  const { id } = req.params
+  const { title } = req.body
+  
+  const project = dbProjects.find(project => project.id === id)
+  
+  project.tasks.push(title)
+  
+  return res.json(dbProjects)  
+
 })
 
 app.put("/projects/:id", reqCount, checkProjectExist, (req, res) => {
+  const { id } = req.params
+  const { title } = req.query
+
+  dbProjects.map(project => {
+    if(project.id === id) {
+      project.title = title
+    }
+
+    return project
+  })
+
+  return res.json( dbProjects )
 
 })
 
 app.delete("/projects/:id", reqCount, checkProjectExist, (req, res) => {
 
+  const { id } = req.params
+  
+  const index = dbProjects.find(project => project.id === id)
+
+  dbProjects.splice(index, 1)
+
+  return res.send()
 })
 
 //middlewares
 function checkProjectExist(req, res, next) {
   
-  const { id } = req.body
+  const { id } = req.params
 
   const project = dbProjects.filter(item => item.id === id)
 
